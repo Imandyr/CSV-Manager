39b3938234540d7a5e8ceb365e81b1e137253774
FedLob <robotreaperdangerkillermaster@gmail.com>
2024-04-30 20:36:31 +0300

    CSVData::ColumnIterator was implemented.

--- csv_data.hpp
+++ csv_data.hpp
@@ -8,7 +8,8 @@
 
 
 using vector_v_s = std::vector<std::vector<std::string>>;
-using u_map_s_ui = std::unordered_map<std::string, uint>;
+using index_type = vector_v_s::size_type;
+using u_map_s_st = std::unordered_map<std::string, index_type>;
 
 
 class CSVData {
@@ -16,6 +17,8 @@ class CSVData {
  *
  */
 public:
+
+    // Helper classes.
 
     class BaseIterator {
     // Base iterator for all CSVData iteators.
@@ -42,26 +45,51 @@ public:
         bool operator!=(BaseIterator& right) {return iter != right.iter;}
     };
 
-//     class ColumnIterator : public BaseIterator {
-//         // Iterator for iteration of values of one column
-//     public:
-//         using BaseIterator::BaseIterator;
-//         ColumnIterator(vector_v_s::iterator it) : iter{it} {}
-//     };
+    class ColumnIterator : public BaseIterator {
+    // Iterator for iteration of values of one column.
+    public:
+        // Returns one std::string.
+        using value_type = std::string;
+        using pointer = value_type*;
+        using reference = value_type&;
 
+        // What values and from which column should it iterate.
+        index_type column_index = 0;
+        ColumnIterator(vector_v_s::iterator it, index_type col) : BaseIterator(it), column_index{col} {}
 
-    u_map_s_ui column_index;
+        // Retrun value of only one column.
+        reference operator*() {return (*iter)[column_index];}
+        reference operator->() {return (*iter)[column_index];}
+    };
+
+    // CSVData class definition.
+
+    // Table with column names and thier indexes in vector.
+    u_map_s_st column_index;
+    // Vector with all data.
     vector_v_s values;
 
-    CSVData(u_map_s_ui c_i = {}, vector_v_s v = {}) {column_index = c_i; values = v;}
+    // Initialization from column table and vector with values.
+    CSVData(u_map_s_st c_i = {}, vector_v_s v = {}) {column_index = c_i; values = v;}
 
-    // TO DO: Remake it into pointer iterator to original objects in values.
+    ColumnIterator begin_col_iter(std::string c_name) {
+        // Returns ColumnIterator on the beginning of a values vector.
+        return ColumnIterator(values.begin(), column_index[c_name]);
+    }
+
+    ColumnIterator end_col_iter(std::string c_name) {
+        // Returns ColumnIterator on the end of a values vector.
+        return ColumnIterator(values.end(), column_index[c_name]);
+    }
+
     std::vector<std::string> column_values(std::string c_name) {
+        // Vector with copies of objects from given column.
         std::vector<std::string> output;
         auto index = column_index[c_name];
         for (auto &i : values) output.push_back(i[index]);
         return output;
     }
+
 
 private:
 
--- tests/test_csv_data.cpp
+++ tests/test_csv_data.cpp
@@ -1,3 +1,6 @@
+// Tests for csv_data.hpp.
+
+
 #include <iostream>
 #include <string>
 #include <stdexcept>
@@ -5,29 +8,51 @@
 #include "../csv_data.hpp"
 
 
+// Prints content of one dimensional sequence.
 template <typename T> void print_1d(T seq) {for (auto &i : seq) std::cout << i << ", "; std::cout << "\n";}
 
 
 void test_of_init() {
+    // Does it initializing correctly?
     CSVData data({{"message", 0}}, {{"text"}});
     if ((data.values[0][0] != "text") || (data.column_index["message"] != 0)) throw std::logic_error("Value was modified.");
 }
 
 
 void test_of_column_values() {
+    // Does CSVData.column_values() returns values of given column name?
     CSVData data({{"message", 0}, {"thing", 1}}, {{"text1", "text2"}, {"text3", "text4"}});
+
     auto out = data.column_values("thing");
-    data.values[0][1] = "new text";
-    std::vector<std::string> example = {"new text", "text4"};
-    print_1d(example); print_1d(out); print_1d(data.values[0]);
+
+    std::vector<std::string> example = {"text2", "text4"};
+
+    // print_1d(example); print_1d(out); print_1d(data.values[0]);
+
     if (out != example) throw std::logic_error("Something wrong with .column_values().");
 }
 
 
+void test_column_iterator() {
+    // Does CSVData::ColumnIterator returns references to original objects from given column?
+    CSVData data({{"message", 0}, {"thing", 1}}, {{"text1", "text2"}, {"text3", "text4"}});
+    for (auto b = data.begin_col_iter("thing"), e = data.end_col_iter("thing"); b != e; ++b) {
+        (*b) = "Changed text.";
+    }
+    // print_1d(data.values[0]); print_1d(data.values[1]);
+    if (data.values[0][1] != "Changed text." || data.values[1][1] != "Changed text.") {
+        throw std::logic_error("Iterator doesn't provide direct references.");
+    }
+
+}
+
+
 int main() {
+    // Runs all tests. If terminates without exceptions - everything good.
 
     test_of_init();
     test_of_column_values();
+    test_column_iterator();
 
     return 0;
 }
