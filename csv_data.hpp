273509d6c6a1c1ef74c586833498de72443071da
FedLob <robotreaperdangerkillermaster@gmail.com>
2024-05-02 20:33:37 +0300

    New methods to CSVData

--- csv_data.hpp
+++ csv_data.hpp
@@ -5,11 +5,9 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <stdexcept>
 
 
-using vector_v_s = std::vector<std::vector<std::string>>;
-using index_type = vector_v_s::size_type;
-using u_map_s_st = std::unordered_map<std::string, index_type>;
 
 
 class CSVData {
@@ -18,7 +16,15 @@ class CSVData {
  */
 public:
 
+    // Type aliases.
+    using vector_s = std::vector<std::string>;
+    using vector_v_s = std::vector<vector_s>;
+    using index_type = vector_v_s::size_type;
+    using u_map_s_st = std::unordered_map<std::string, index_type>;
+
+
     // Helper classes.
+
 
     class BaseIterator {
     // Base iterator for all CSVData iteators.
@@ -26,7 +32,7 @@ public:
         // Iterator tags.
         using iterator_category = std::forward_iterator_tag;
         using difference_type = std::ptrdiff_t;
-        using value_type = std::vector<std::string>;
+        using value_type = vector_s;
         using pointer = value_type*;
         using reference = value_type&;
 
@@ -45,6 +51,7 @@ public:
         bool operator!=(BaseIterator& right) {return iter != right.iter;}
     };
 
+
     class ColumnIterator : public BaseIterator {
     // Iterator for iteration of values of one column.
     public:
@@ -62,33 +69,136 @@ public:
         reference operator->() {return (*iter)[column_index];}
     };
 
+
+    class Column {
+    /* Column represents one column from data. It not stores column values, but provides .begin() and .end() methods,
+     * which returns iterators for iteration of values from target column.
+     */
+    private:
+        // Pointer to original data and index of target column.
+        vector_v_s* target;
+        index_type column_index = 0;
+
+    public:
+        // Initialization with pointer to target vector and index of target column.
+        Column(vector_v_s* vec, index_type col) : target{vec}, column_index{col} {}
+
+        ColumnIterator begin() {
+            // Returns ColumnIterator on the beginning of a values vector.
+            return ColumnIterator(target->begin(), column_index);
+        }
+
+        ColumnIterator end() {
+            // Returns ColumnIterator on the end of a values vector.
+            return ColumnIterator(target->end(), column_index);
+        }
+    };
+
+
     // CSVData class definition.
+
 
     // Table with column names and thier indexes in vector.
     u_map_s_st column_index;
     // Vector with all data.
     vector_v_s values;
 
-    // Initialization from column table and vector with values.
-    CSVData(u_map_s_st c_i = {}, vector_v_s v = {}) {column_index = c_i; values = v;}
-
-    ColumnIterator begin_col_iter(std::string c_name) {
-        // Returns ColumnIterator on the beginning of a values vector.
-        return ColumnIterator(values.begin(), column_index[c_name]);
+    CSVData(u_map_s_st c_i = {}, vector_v_s v = {}) {
+        // Initialization from column table and vector with values.
+        column_index = c_i;
+        values = v;
     }
 
-    ColumnIterator end_col_iter(std::string c_name) {
-        // Returns ColumnIterator on the end of a values vector.
-        return ColumnIterator(values.end(), column_index[c_name]);
+
+    vector_v_s::iterator begin() {
+        return values.begin();
+    }
+    vector_v_s::iterator end() {
+        return values.end();
+    }
+    vector_s operator[](index_type index) {
+        return values[index];
     }
 
-    std::vector<std::string> column_values(std::string c_name) {
-        // Vector with copies of objects from given column.
-        std::vector<std::string> output;
-        auto index = column_index[c_name];
-        for (auto &i : values) output.push_back(i[index]);
-        return output;
+
+    Column column(std::string name) {
+        // Creates Column object of column with target name.
+        return Column(&values, column_index[name]);
     }
+
+
+    void delete_row(index_type index) {
+        // Deletes row on given index.
+        values.erase(values.begin() + index);
+    }
+    void delete_row(index_type from, index_type number) {
+        // Deletes number of rows after "from" index (including "from").
+        auto start = values.begin() + from;
+        values.erase(start, start + number);
+    }
+
+
+    void add_row(index_type number) {
+        // Adds "number" blank rows at the end of the values vector.
+        vector_s blank(column_index.size(), "");
+        for (index_type i = 0; i < number; ++i)
+            values.push_back(blank);
+    }
+    void add_row(index_type number, vector_s row) {
+        // Adds "number" rows with value "row" at the end of the values vector.
+        for (index_type i = 0; i < number; ++i)
+            values.push_back(row);
+    }
+
+
+    void insert_row(index_type to) {
+        // Inserts blank row on index before "to".
+        values.insert(values.begin() + to, vector_s(column_index.size(), ""));
+    }
+    void insert_row(index_type to, index_type number) {
+        // Inserts number of blank rows on index before "to".
+        values.insert(values.begin() + to, number, vector_s(column_index.size(), ""));
+    }
+    void insert_row(index_type to, vector_s row) {
+        // Inserts row with given value on index before "to".
+        values.insert(values.begin() + to, row);
+    }
+    void insert_row(index_type to, vector_s row, index_type number) {
+        // Inserts number of rows with given value on index before "to".
+        values.insert(values.begin() + to, number, row);
+    }
+    void insert_row(index_type to, std::initializer_list<vector_s> rows) {
+        // Inserts rows on index of "to".
+        values.insert(values.begin() + to, rows);
+    }
+
+
+    void add_column(std::string name, std::string value = "") {
+        // Adds a column at the end with the name "name" and appends the value to every row, so a number of columns and elements matches.
+        // Raises std::invalid_argument if the column name is already exist.
+
+        if (column_index.find(name) != column_index.end())
+            throw std::invalid_argument("A column with name \"" + name + "\" already exists.");
+
+        column_index[name] = column_index.size();
+        for (auto &i : values)
+            i.push_back(value);
+    }
+
+
+    void delete_column(std::string name) {
+        // Deletes column and all its values. Raises std::invalid_argument if there is no such column.
+
+        if (column_index.find(name) == column_index.end())
+            throw std::invalid_argument("A column with name \"" + name + "\" doesn't exists.");
+
+        auto index = column_index[name];
+        column_index.erase(name);
+
+        for (auto &i : values)
+            i.erase(i.begin() + index);
+    }
+
 
 
 private:
--- tests/test_csv_data.cpp
+++ tests/test_csv_data.cpp
@@ -8,41 +8,187 @@
 #include "../csv_data.hpp"
 
 
-// Prints content of one dimensional sequence.
-template <typename T> void print_1d(T seq) {for (auto &i : seq) std::cout << i << ", "; std::cout << "\n";}
+
+template <typename T> void print_1d(T seq) {
+    // Prints content of one dimensional sequence.
+    for (auto &i : seq) std::cout << i << ", "; std::cout << "\n";
+}
+template <typename T> void print_2d(T seq) {
+    // Prints content of two dimensional sequence.
+    for (auto &i : seq) {
+        print_1d(i);
+    }
+    std::cout << "\n";
+}
 
 
 void test_of_init() {
-    // Does it initializing correctly?
+    // Does CSVData initializing correctly?
+
     CSVData data({{"message", 0}}, {{"text"}});
-    if ((data.values[0][0] != "text") || (data.column_index["message"] != 0)) throw std::logic_error("Value was modified.");
+
+    if ((data.values[0][0] != "text") || (data.column_index["message"] != 0))
+        throw std::logic_error("Value was modified.");
 }
 
 
-void test_of_column_values() {
-    // Does CSVData.column_values() returns values of given column name?
+void test_column() {
+    // Does .column() return references to the original objects from given column?
+
     CSVData data({{"message", 0}, {"thing", 1}}, {{"text1", "text2"}, {"text3", "text4"}});
 
-    auto out = data.column_values("thing");
-
-    std::vector<std::string> example = {"text2", "text4"};
-
-    // print_1d(example); print_1d(out); print_1d(data.values[0]);
-
-    if (out != example) throw std::logic_error("Something wrong with .column_values().");
-}
-
-
-void test_column_iterator() {
-    // Does CSVData::ColumnIterator returns references to original objects from given column?
-    CSVData data({{"message", 0}, {"thing", 1}}, {{"text1", "text2"}, {"text3", "text4"}});
-    for (auto b = data.begin_col_iter("thing"), e = data.end_col_iter("thing"); b != e; ++b) {
-        (*b) = "Changed text.";
+    for (auto &i : data.column("thing")) {
+        i = "Changed text.";
     }
     // print_1d(data.values[0]); print_1d(data.values[1]);
+
     if (data.values[0][1] != "Changed text." || data.values[1][1] != "Changed text.") {
         throw std::logic_error("Iterator doesn't provide direct references.");
     }
+}
+
+
+void test_delete_row_1() {
+    // Does .delete_row(1, 2) delete elements correctly?
+    CSVData data(
+        {{"message", 0}, {"thing", 1}, {"third", 2}},
+        {{"text1", "text2", "what"}, {"text3", "text4", "where"},
+         {"text5", "text6", "when"}, {"who", "how", "why"}}
+    );
+    // print_2d(data.values);
+    data.delete_row(1, 2);
+    // print_2d(data.values);
+    if (data.values[0][0] != "text1" || data.values[1][0] != "who") {
+        throw std::logic_error(".delete_row(1, 2) doesn't work as inteded.");
+    }
+}
+
+
+void test_delete_row_2() {
+    // Does .delete_row(1) delete element correctly?
+
+    CSVData data({{"message", 0}}, {{"text3"}, {"text5"}, {"who"}});
+
+    //print_2d(data.values);
+    data.delete_row(1);
+    //print_2d(data.values);
+
+    if (data.values[0][0] != "text3" || data.values[1][0] != "who")
+        throw std::logic_error(".delete_row(1) doesn't work as inteded.");
+
+    data.delete_row(1);
+}
+
+
+void test_delete_row_3() {
+    // What happens if you try to delete a non-existing row?
+}
+
+
+void test_add_row_1() {
+    // Test of addition of n rows using .add_row(3).
+
+    CSVData data({{"message", 0}, {"number", 1}, {"thing", 2}, {"yeah", 3}}, {{"1", "2", "3", "4"}});
+    std::vector<std::string> blank(4, "");
+
+    data.add_row(3);
+
+    if (data.values.size() != 4)
+        throw std::logic_error(".add_row(3) hasn't added 3 rows.");
+    if (data.values[0][0] != "1")
+        throw std::logic_error(".add_row(3) changed first rows.");
+
+    // print_2d(data.values);
+    for (auto begin = data.begin() + 1, end = data.end(); begin != end; ++begin)
+        if (*begin != blank)
+            throw std::logic_error(".add_row(3) added invalid rows.");
+}
+
+
+void test_add_row_2() {
+    // Test of addition of n rows using .add_row(number, row).
+
+    CSVData data({{"message", 0}, {"number", 1}, {"thing", 2}, {"yeah", 3}}, {{"1", "2", "3", "4"}});
+    std::vector<std::string> row = {"4", "3", "2", "1"};
+
+    data.add_row(3, {"4", "3", "2", "1"});
+
+    if (data.values.size() != 4)
+        throw std::logic_error(".add_row(number, row) hasn't added 3 rows.");
+    if (data.values[0][0] != "1")
+        throw std::logic_error(".add_row(number, row) changed first rows.");
+
+    // print_2d(data.values);
+    for (auto begin = data.begin() + 1, end = data.end(); begin != end; ++begin)
+        if (*begin != row)
+            throw std::logic_error(".add_row(number, row) added invalid rows.");
+}
+
+
+void test_insert_row() {
+    // Test of all variants of .insert_row().
+
+    CSVData::vector_s first = {"1", "2", "3", "4"}, last = {"56", "23", "34", "54"};
+    std::vector<std::string> row = {"4", "3", "2", "1"};
+    std::vector<std::string> blank(4, "");
+
+    CSVData data({{"message", 0}, {"number", 1}, {"thing", 2}, {"yeah", 3}}, {first, last});
+
+    data.insert_row(1); // 2 blank on 1 index.
+    data.insert_row(2, 2); // 2 blanks on 2 index.
+    data.insert_row(4, row); // Row on 4 index.
+    data.insert_row(5, row, 2); // 2 rows on 4 index.
+    data.insert_row(7, {{"4", "21", "53", "24"}, row}); // {"4", "21", "53", "24"} and row on 7 index.
+    //print_2d(data.values);
+
+    if (
+        data.values[0] != first || data.values[9] != last ||
+        data.values[1] != blank || data.values[2] != blank || data.values[3] != blank ||
+        data.values[4] != row || data.values[5] != row || data.values[6] != row ||
+        data.values[7] != CSVData::vector_s{"4", "21", "53", "24"} || data.values[8] != row
+    ) throw std::logic_error(".insert_row() isn't working right.");
+}
+
+
+void test_add_column() {
+    // Test of .add_column() method.
+
+    CSVData data({{"message", 0}}, {{"4"}});
+
+    data.add_column("who", "123");
+
+    if (data.column_index["who"] != 1 || data.values[0] != CSVData::vector_s{"4", "123"})
+        throw std::logic_error(".add_column() isn't working right.");
+
+    bool bad = true;
+    try {data.add_column("who");}
+    catch (std::invalid_argument) {bad = false;}
+
+    if (bad)
+        throw std::logic_error(".add_column() isn't detecting rewrite of already existing column.");
+}
+
+
+void test_delete_column() {
+    // Test of column deletion.
+
+    CSVData::vector_s first = {"1", "2", "3", "4"}, last = {"56", "23", "34", "54"};
+    CSVData data({{"message", 0}, {"number", 1}, {"thing", 2}, {"yeah", 3}}, {first, last});
+
+    data.delete_column("thing");
+    // print_2d(data);
+
+    if (
+        data.column_index.find("thing") != data.column_index.end() ||
+        data[0][1] != "2" || data[0][2] != "4" || data[1][1] != "23" || data[1][2] != "54"
+    ) throw std::logic_error(".delete_column() Doesn't delete things correctly.");
+
+    bool bad = true;
+    try {data.delete_column("thing");}
+    catch (std::invalid_argument) {bad = false;}
+
+    if (bad)
+        throw std::logic_error(".delete_column() deletes non-existing column.");
 
 }
 
@@ -51,8 +197,21 @@ int main() {
     // Runs all tests. If terminates without exceptions - everything good.
 
     test_of_init();
-    test_of_column_values();
-    test_column_iterator();
+
+    test_column();
+
+    test_delete_row_1();
+    test_delete_row_2();
+    test_delete_row_3();
+
+    test_add_row_1();
+    test_add_row_2();
+
+    test_insert_row();
+
+    test_add_column();
+
+    test_delete_column();
 
     return 0;
 }
